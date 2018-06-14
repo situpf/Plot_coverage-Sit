5a617229f648f9d3178230c03c435fbe2699d220
Winona Oliveros Diez <35917727+wod31@users.noreply.github.com>
2018-06-14 13:10:15 +0200

    Update coverage_in_windows_singlepage.R

--- coverage_in_windows_singlepage.R
+++ coverage_in_windows_singlepage.R
@@ -6,7 +6,7 @@
 args <- commandArgs(trailingOnly=TRUE) #collect data from arguments
 win <- args[1]
 filein <- args[2]
-output_dir <- args[3]
+name <- args[3]
 
 cov <- read.table(filein, header = F, sep = "\t")
 colnames(cov) <- c("chr","start","end","value")
@@ -16,7 +16,7 @@ total_chr <- levels(cov$chr)
 total_chr <- c(1:22,"X","Y","MT")
 max_length <- max(cov$end)
 
-pdf(sprintf("%stest_%s_singlepage.pdf", output_dir, win), width = 15, height = 125)
+pdf(sprintf("%s.pdf", name), width = 15, height = 125)
 par(mfrow=c(25,1))
 for (i in total_chr){
   print(i)
